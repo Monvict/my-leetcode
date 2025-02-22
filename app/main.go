4f673b94d3a9ab38caf7021022f3fc1af5301171
lotus <caomonvic@gmail.com>
2025-02-22 23:14:19 +0800

    459 repeat string

--- app/main.go
+++ app/main.go
@@ -1,20 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	s := "Hello"
-	r := []byte{}
-	for _, c := range s {
-		charByte := byte(c)
-		fmt.Println("charByte = ", charByte)
-		if charByte >= 65 && charByte <= 90 {
-			charByte = charByte + 32
-		}
+	s := "abcde"
 
-		r = append(r, charByte)
-	}
-
-	fmt.Println(r)
-	fmt.Println(string(r))
+	fmt.Println(s[0:2])
+	fmt.Println(s[2:4])
 }
