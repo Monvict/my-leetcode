b37312eb44f6886a328f0cd9abbc5546f59a9a7d
lotus <caomonvic@163.com>
2025-03-21 22:18:13 +0800

    121 best time to buy and sale stock --my solution

---
+++ go/0121.best-time-to-buy-and-sell-stock/solution.go
@@ -0,0 +1,48 @@
+// Created by lotus at 2025/03/21 21:53
+// leetgo: 1.4.13
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+package main
+
+import (
+	"fmt"
+)
+
+// @lc code=begin
+
+func maxProfit(prices []int) (ans int) {
+	// buyPrice := prices[0]
+	// bueIndex := 0
+	// for i := 1; i < len(prices); i++ {
+	// 	if prices[i] < buyPrice {
+	// 		buyPrice = prices[i]
+	// 		bueIndex = i
+	// 	}
+	// }
+
+	// salePrice := buyPrice
+	// for j := bueIndex; j < len(prices); j++ {
+	// 	if prices[j] > salePrice {
+	// 		salePrice = prices[j]
+	// 	}
+	// }
+
+	profit := 0
+
+	for i := 0; i < len(prices); i++ {
+		for j := i + 1; j < len(prices); j++ {
+			delta := prices[j] - prices[i]
+			if delta > profit {
+				profit = delta
+			}
+		}
+	}
+	return profit
+}
+
+// @lc code=end
+
+func main() {
+	nums := []int{7, 6, 4, 3, 1}
+	fmt.Println(maxProfit(nums))
+}
